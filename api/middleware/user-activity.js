72c5a056a0dba98302fa3645bacfee7e4776dbd3
CHAMARA-PC\Asus <sarathchandra.chamara@gmail.com>
2020-08-04 23:05:32 +0530

    add addtional charges to the user activities

--- api/middleware/user-activity.js
+++ api/middleware/user-activity.js
@@ -3,6 +3,9 @@ const mongoose = require('mongoose');
 
 exports.create_records = (req, res, next) => {
     console.log("userObj*******", req.body);
+    var additional_charges = req.body.additional_charges;
+    var interest = req.body.interest_paid;
+    var interest_paid = additional_charges + interest;
     const userActivitySchema = new UserActivity({
         _id: new mongoose.Types.ObjectId(),
         user: req.body.userId,
@@ -10,7 +13,7 @@ exports.create_records = (req, res, next) => {
         phone_number: req.body.phone_number,
         amount: req.body.amount,
         additional_amount: req.body.additional_amount,
-        interest_paid: req.body.interest_paid + req.body.additional_charges,
+        interest_paid: interest_paid,
         article_status: req.body.article_status,
         released_amount: req.body.released_amount,
     });
