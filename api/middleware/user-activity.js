15c915eb08769eb30d6426ff492b56dbcdbf1bdd
CHAMARA-PC\Asus <sarathchandra.chamara@gmail.com>
2020-08-04 23:09:54 +0530

    add addtional charges to the user activities

--- api/middleware/user-activity.js
+++ api/middleware/user-activity.js
@@ -13,7 +13,7 @@ exports.create_records = (req, res, next) => {
         phone_number: req.body.phone_number,
         amount: req.body.amount,
         additional_amount: req.body.additional_amount,
-        interest_paid: interest_paid,
+        interest_paid: req.body.interest_paid,
         article_status: req.body.article_status,
         released_amount: req.body.released_amount,
     });
